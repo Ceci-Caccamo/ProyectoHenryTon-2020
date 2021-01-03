eb8ed6ea6936d04e8d665a8376a33ab3bbf40768
Lucas G <g.brianlucas1993@gmail.com>
2021-01-03 19:26:28 -0300

    edited about us

--- cliente/src/components/pages/AboutUs.js
+++ cliente/src/components/pages/AboutUs.js
@@ -12,23 +12,28 @@ export default function AboutUs(){
                 <img className={s.productImg} src='images/img-19.jpg' alt='img' />
                 <div className={s.productInfo}>
                     <p>
-                        <span className={s.productData}>EduTiva:</span>
-                            Viene de EDUcación colaboraTIVA.
+                        <span className={s.productData}>Nombre:</span>
+                            EduTiva viene de EDUcación colaboraTIVA.
                     </p>
                     <br />
                     <p>
-                        <span className={s.productData}>Objetivo:</span>
-                            Concientizar ante catástrofes por medio de la colaboración educativa.
+                        <span className={s.productData}>Misión:</span>
+                            Concientizar ante catástrofes por medio de una plataforma, el cual permita subir contenido a cualquier persona o entidad, donde se promueva la colaboración educativa pudiéndose también expedir certificados.
                     </p>
                     <br />
                     <p>
-                        <span className={s.productData}>Descripción:</span>
-                        Ante la problemática del aumento constante de catástrofes y la desinformación de la población en general sentimos la necesidad de una plataforma en la cual, desde personas a entidades, puedan colaborar subiendo contenido con el objetivo de concientizar, educar y reducir muertes por catástrofes.
+                        <span className={s.productData}>Visión:</span>
+                            Reducir los daños y evitar pérdidas, producidas por catástrofes naturales y problemáticas previsibles, tales como daños psicológicos, trastornos a causa de ineficiencia, muertes, bienes, etc. Haciendo así, a la plataforma una gran red de educación, información y colaboración para todo el mundo.
+                    </p>
+                    <br />
+                    <p>
+                        <span className={s.productData}>Valores:</span>
+                            Educación, colaboración, concientización.
                     </p>
                     <br />
                     <p>
                         <span className={s.productData}>Equipo:</span>
-                            Lucas Gualampa, Julian Scicolone, Ceci-Caccamo, Leonardo Zambrano y Felipe Blanco.
+                            Lucas Gualampa, Felipe Blanco, Ceci Caccamo, Leonardo Zambrano y Julian Scicolone.
                     </p>      
                 </div>
             </div>
