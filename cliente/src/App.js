a86188a0035f2ade3379a1ee480f9d3834ad5939
lzambranoa <lzambranoa.software@gmail.com>
2021-01-03 18:26:15 -0500

    Detalle de los cursos

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -8,6 +8,7 @@ import AboutUs from './components/pages/AboutUs';
 import Education from './components/pages/Education';
 import Blog from './components/pages/Blog';
 import SignUp from './components/pages/SignUp';
+import { CourseScreen } from './components/courses/CourseScreen';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route path='/AboutUs' component={AboutUs} />
           <Route path='/Education' component={Education} />
           <Route path='/Blog' component={Blog} />
+          <Route exact path="/curso/:cursoId" component={ CourseScreen } /> 
           <Route path='/SignUp' component={SignUp} />
         </Switch>
         <Footer />
